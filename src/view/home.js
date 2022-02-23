d78af8aab435da0b494b84ca0d7b97bc0d2e6166
Naimerith <naimerith89@gmail.com>
2022-02-22 20:51:47 -0500

    Se agregaron estilos a diseño mobile

--- src/view/home.js
+++ src/view/home.js
@@ -1,4 +1,4 @@
-import { closeUserSession, addPost, realTimePosts, getPosts } from '../firebase/firebaseFunciones.js'
+import { closeUserSession, addPost, realTimePosts, likes, removeLikes, getDataUser } from '../firebase/firebaseFunciones.js'
 import { user } from '../firebase/config.js'
 import { templateFooter } from './footer.js'
 import { templateHeader } from './header.js'
@@ -8,8 +8,8 @@ export const home = () => {
     const templateHome = `
     <section class="homePage">
     <div class="fotoPerfil">
-    <h5 id="userName"></h5>
     <img src="" alt="foto" id="photoURL">
+    <h5 class="userName" id="userName"></h5>
     </div>
     <div class="insertarPublicacion">
         <input id="insertarPublicacion" class="inputInsertarPublicacion" name="insertarPublicacion" type="text" placeholder="¿Que quieres compartir?"/><br>
@@ -21,17 +21,17 @@ export const home = () => {
     <section id="insertPost">
     </section>
     `
-    function templatePost (photoUser, nameUser, datePost, postUser, idDoc, countLike) {
-    const publish= homePage.querySelector('#insertPost');
-    publish.innerHTML +=
-    `<div class="container_post">
+    function templatePost(photoUser, nameUser, datePost, postUser, idDoc, cuentaLike) {
+        const publish = homePage.querySelector('#insertPost');
+        publish.innerHTML +=
+            `<div class="container_post">
         <div class="header_post">
             <aside class="title_post">
                 <h1 class="title">Publicado por: ${nameUser} </h1>
                 <h4 class="date_hour">${datePost}</h4>
             </aside>
             <aside class="photo_perfil">
-                <img src="${photoUser}" alt="foto">
+                <img class="photo_public" src="${photoUser}" alt="foto">
             </aside>
         </div>
         <div class="content_post">
@@ -39,7 +39,8 @@ export const home = () => {
         </div>
         <div class="iconos">
             <aside class="icons_iteration">
-                <img src="" alt="like">
+                <img id='btn_give_like' class='mark_like' src="./img/like.png" alt="like">
+                <p class="counterlike">${cuentaLike}</p>
                 <img src="" alt="coment">
             </aside>
             <aside class="icons_iteration">
@@ -48,36 +49,36 @@ export const home = () => {
             </aside>
         </div>
         <div class="coment">
-            <input id="insert_coment" class="insert_coment" name="coment" type="text" placeholder="Añadir Comentario"/>
+            <p></p>
+            <input id="insert_coment" class="insert_coment" name="coment" type="text" placeholder="Añadir un comentario..."/>
             <button type="submit" class="btn-comentar" id="btn-comentar">
                 Publicar
             </button>
         </div>
     </div>`;
-    return publish;
+        return publish;
     };
 
     const homePage = document.createElement('article');
     homePage.classList.add('article-home');
     homePage.innerHTML = templateHeader + templateHome + templateFooter
 
-    //const userName= homePage.querySelector('#userName');
-    
     //botón de publicar
     const publishButton = homePage.querySelector('#btn-publicar');
     publishButton.addEventListener('click', () => {
-        const contentPost= document.querySelector('#insertarPublicacion');
-        if(contentPost.value !== ''){
+        const contentPost = document.querySelector('#insertarPublicacion');
+        if (contentPost.value !== '') {
             addPost(user().displayName, contentPost.value, user().photoURL, user().uid);
         }
         contentPost.value = '';
     });
-     
+
     // mostrar las publicaciones 
-    const publish= homePage.querySelector('#insertPost');
+    const publish = homePage.querySelector('#insertPost');
     realTimePosts((querySnapshot) => {
         publish.innerHTML = '';
         querySnapshot.forEach((doc) => {
+            //console.log(doc.data());
             const fotoUser = doc.data().userPhotoPost;
             const nombreUser = doc.data().userWhoPublishes;
             const fechaPost = doc.data().publicationDate;
@@ -85,24 +86,58 @@ export const home = () => {
             const idUsuario = user().uid;
             const cuentaLike = doc.data().likesPost;
             const idDocumento = doc.id;
-            const btnHeart = (contadorLike.indexOf(idUsuario) !== -1) ? 'painted' : '';
-        templatePost(fotoUser, nombreUser, fechaPost, textoPost, idDocumento, cuentaLike.length, btnHeart);
-     });
-    });
-    console.log(realTimePosts)
+            const btnHeart = (cuentaLike.indexOf(idUsuario) !== -1) ? 'paint' : '';
+            templatePost(fotoUser, nombreUser, fechaPost, textoPost, idDocumento, cuentaLike.length, btnHeart);
 
-    /************Cerrar sesión Usuario**************/
-    const logOut = homePage.querySelector('#logOut');
-    logOut.addEventListener('click', () => {
-        closeUserSession()
-            .then(() => {
-                console.log('El usuario ha cerrado sesión');
-                window.location.hash = '#/';
-            })
-            .catch((error) => {
-                console.log(error, 'No se pudo cerrar la sesión');
+            /************likes a las publicaciones **************/
+            const btn_give_like = homePage.querySelectorAll('.mark_like');
+            //console.log(btn_give_like); //Selecciona todos los likes de todas las publicaciones, se crea un Nodelist [] OJO esta en 0
+            btn_give_like.forEach((like) => {
+                like.addEventListener('click', (e) => {
+                    console.log('diste click')
+                    const idPost = e.target.dataset.id;
+                    if (e.target.classList.contains('paint')) {
+                        removeLikes(idPost, idUsuario).FieldValue;
+                        e.target.classList.remove('paint')
+                        console.log("se quito el like");
+                    } else {
+                        likes(idPost, idUsuario).FieldValue;
+                        e.target.classList.add('paint')
+                        console.log("se dio like");
+                    }
+
+                });
             });
-    });
+        });
+        //console.log(realTimePosts)
 
+        /************Insertar nombre de usuario y Foto**************/
+        const loginUsername = homePage.querySelector('#userName');
+        const photoUsername = homePage.querySelector('#photoURL');
+        getDataUser()
+            .then((querySnapshot) => {
+                querySnapshot.forEach((doc) => {
+                    if (user().uid === doc.data().IdUserActive) {
+                        loginUsername.textContent = doc.data().nameUser;
+                        photoUsername.src = doc.data().photoGmail;
+                    }
+                });
+            });
+
+        /************Cerrar sesión Usuario**************/
+        const logOut = homePage.querySelector('#logOut');
+        logOut.addEventListener('click', () => {
+            closeUserSession()
+                .then(() => {
+                    console.log('El usuario ha cerrado sesión');
+                    window.location.hash = '#/';
+                })
+                .catch((error) => {
+                    console.log(error, 'No se pudo cerrar la sesión');
+                });
+        });
+
+
+    })
     return homePage;
-};
+}
